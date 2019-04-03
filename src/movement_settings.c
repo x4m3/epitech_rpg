7893e7992467bc5adf749bf69e159194c341a257
kenny.buquen <kenny.buquen@epitech.eu>
2019-04-03 12:49:34 +0200

    rm useless files and add assets

---
+++ src/movement_settings.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** 2018
+** File description:
+** __DESCRIPTION__
+*/
+
+#include "my_rpg.h"
+
+pos_t get_input(void)
+{
+    pos_t res = OFF;
+
+    (sfKeyboard_isKeyPressed(sfKeyUp) == sfTrue) ? (res = UP) : (0);
+    (sfKeyboard_isKeyPressed(sfKeyDown) == sfTrue) ? (res = DOWN) : (0);
+    (sfKeyboard_isKeyPressed(sfKeyLeft) == sfTrue) ? (res = LEFT) : (0);
+    (sfKeyboard_isKeyPressed(sfKeyRight) == sfTrue) ? (res = RIGHT) : (0);
+    return (result);
+}

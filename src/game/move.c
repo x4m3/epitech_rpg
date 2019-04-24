77eddb1e4fb8d2c865fa2b261b4f973baefd3be0
Alexis Riot <riotalexis35@gmail.com>
2019-04-24 08:37:40 +0200

    #7 - Fix player who can move out the map

--- src/game/move.c
+++ src/game/move.c
@@ -18,8 +18,10 @@ void char_movement_right(env_t *env)
     if (pos.x < (env->game_s.map_size.width - (1920 / 2) * 0.6)
     && pos.x == env->game_s.p_character.x)
         sfView_move(env->game_s.view, (sfVector2f) {4, 0});
-    set_character_animation(env);
-    env->game_s.p_character.x += 4;
+    if (env->game_s.p_character.x + (32 / 2) < env->game_s.map_size.width) {
+        set_character_animation(env);
+        env->game_s.p_character.x += 4;
+    }
 }
 
 void char_movement_left(env_t *env)
@@ -32,8 +34,11 @@ void char_movement_left(env_t *env)
     if (pos.x > (1920 / 2) * 0.6
     && pos.x == env->game_s.p_character.x)
         sfView_move(env->game_s.view, (sfVector2f) {-4, 0});
-    set_character_animation(env);
-    env->game_s.p_character.x -= 4;
+
+    if (env->game_s.p_character.x - (32 / 2) > 0) {
+        set_character_animation(env);
+        env->game_s.p_character.x -= 4;
+    }
 }
 
 void char_movement_top(env_t *env)
@@ -46,8 +51,10 @@ void char_movement_top(env_t *env)
     if (pos.y > (1080 / 2) * 0.6
     && pos.y == env->game_s.p_character.y)
         sfView_move(env->game_s.view, (sfVector2f) {0, -4});
-    set_character_animation(env);
-    env->game_s.p_character.y -= 4;
+    if (env->game_s.p_character.y - (32 / 2) > 0) {
+        set_character_animation(env);
+        env->game_s.p_character.y -= 4;
+    }
 }
 
 void char_movement_down(env_t *env)
@@ -60,6 +67,8 @@ void char_movement_down(env_t *env)
     if (pos.y < (env->game_s.map_size.height - (1080 / 2) * 0.6)
     && pos.y == env->game_s.p_character.y)
         sfView_move(env->game_s.view, (sfVector2f) {0, 4});
-    set_character_animation(env);
-    env->game_s.p_character.y += 4;
+    if (env->game_s.p_character.y + (32 / 2) < env->game_s.map_size.height) {
+        set_character_animation(env);
+        env->game_s.p_character.y += 4;
+    }
 }
